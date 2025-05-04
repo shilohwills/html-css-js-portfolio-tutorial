deaaec76f2badba37536b8273b0175d2900f88fc
shilohwills <shilohegbuji874@gmail.com>
2025-05-04 16:05:46 +0100

    updated hamburger menu

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-const hamburger = document.getElementById('hamburger');
-const menuLinks = document.getElementById('menu-links');
+function toggleMenu() {
+  const hamburger = document.querySelector('.js-hamburger');
+const menuLinks = document.querySelector('.js-menu-links');
 
 hamburger.addEventListener('click', () => {
-  // Toggle menu visibility
-  menu-links.classList.toggle('active');
-  
-  // Toggle hamburger animation
-  hamburger.classList.toggle('active');
-});
+  menuLinks.classList.toggle('open');
+  });
+}
